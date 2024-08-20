b020143074a54c801c7d6b371475138d329d2427
Burger-karl <charleskalu863@gmail.com>
2024-08-20 19:10:55 +0100

    deployment changes

--- subscriptions/apps.py
+++ subscriptions/apps.py
@@ -1,8 +1,26 @@
+# from django.apps import AppConfig
+
+# class SubscriptionsConfig(AppConfig):
+#     name = 'subscriptions'
+
+#     def ready(self):
+#         from .models import SubscriptionPlan
+#         SubscriptionPlan.create_default_plans()
+
+
 from django.apps import AppConfig
+from django.db.utils import OperationalError
 
 class SubscriptionsConfig(AppConfig):
     name = 'subscriptions'
 
     def ready(self):
         from .models import SubscriptionPlan
-        SubscriptionPlan.create_default_plans()
+        from . import signals  # Ensure signals are connected
+
+        try:
+            # Attempt to query to check if the table exists
+            SubscriptionPlan.objects.get(name='free')
+        except OperationalError:
+            # The table doesn't exist yet, so handle it gracefully
+            pass
--- subscriptions/signals.py
+++ subscriptions/signals.py
@@ -1,10 +1,27 @@
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.conf import settings
+# from .models import UserSubscription, SubscriptionPlan
+
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_user_subscription(sender, instance, created, **kwargs):
+#     if created:
+#         free_plan = SubscriptionPlan.objects.get(name=SubscriptionPlan.FREE)
+#         UserSubscription.objects.create(user=instance, plan=free_plan, subscription_status='inactive')
+
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from .models import UserSubscription, SubscriptionPlan
+from django.db.utils import OperationalError
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_subscription(sender, instance, created, **kwargs):
     if created:
-        free_plan = SubscriptionPlan.objects.get(name=SubscriptionPlan.FREE)
-        UserSubscription.objects.create(user=instance, plan=free_plan, subscription_status='inactive')
+        try:
+            free_plan = SubscriptionPlan.objects.get(name=SubscriptionPlan.FREE)
+            UserSubscription.objects.create(user=instance, plan=free_plan, subscription_status='inactive')
+        except OperationalError:
+            # Handle the case where the SubscriptionPlan table does not exist yet
+            pass
