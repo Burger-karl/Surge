74a1765c8cdfb9f88c680277433134c947aa64a6
Burger-karl <charleskalu863@gmail.com>
2024-08-21 11:48:21 +0100

    apps changes

--- booking/apps.py
+++ booking/apps.py
@@ -1,7 +1,15 @@
+# from django.apps import AppConfig
+
+# class BookingConfig(AppConfig):
+#     name = 'booking'
+
+#     def ready(self):
+#         import booking.signals
+
+
 from django.apps import AppConfig
 
-class BookingConfig(AppConfig):
-    name = 'booking'
 
-    def ready(self):
-        import booking.signals
+class BookingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'booking'
--- subscriptions/apps.py
+++ subscriptions/apps.py
@@ -1,26 +1,24 @@
+from django.apps import AppConfig
+
+
+class SubscriptionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'subscriptions'
+
+
 # from django.apps import AppConfig
+# from django.db.utils import OperationalError
 
 # class SubscriptionsConfig(AppConfig):
 #     name = 'subscriptions'
 
 #     def ready(self):
 #         from .models import SubscriptionPlan
-#         SubscriptionPlan.create_default_plans()
+#         from . import signals  # Ensure signals are connected
 
-
-from django.apps import AppConfig
-from django.db.utils import OperationalError
-
-class SubscriptionsConfig(AppConfig):
-    name = 'subscriptions'
-
-    def ready(self):
-        from .models import SubscriptionPlan
-        from . import signals  # Ensure signals are connected
-
-        try:
-            # Attempt to query to check if the table exists
-            SubscriptionPlan.objects.get(name='free')
-        except OperationalError:
-            # The table doesn't exist yet, so handle it gracefully
-            pass
+#         try:
+#             # Attempt to query to check if the table exists
+#             SubscriptionPlan.objects.get(name='free')
+#         except OperationalError:
+#             # The table doesn't exist yet, so handle it gracefully
+#             pass
