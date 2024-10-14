dade2791697f8c1be52c6ad2dc0e304b67add824
Burger-karl <charleskalu863@gmail.com>
2024-10-14 14:45:35 +0100

    final changes made

--- booking/apps.py
+++ booking/apps.py
@@ -1,15 +1,15 @@
-# from django.apps import AppConfig
-
-# class BookingConfig(AppConfig):
-#     name = 'booking'
-
-#     def ready(self):
-#         import booking.signals
-
-
 from django.apps import AppConfig
 
-
 class BookingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking'
+
+    def ready(self):
+        import booking.signals
+
+
+# from django.apps import AppConfig
+
+
+# class BookingConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'booking'
